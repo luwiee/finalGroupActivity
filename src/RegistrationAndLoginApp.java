826ed13c1409f31a7f9308744d054d6be5f1b16f
Lawrence <46209705+luwiee@users.noreply.github.com>
2022-05-05 17:40:30 +0800

    Added base gui

--- src/RegistrationAndLoginApp.java
+++ src/RegistrationAndLoginApp.java
@@ -1,2 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class RegistrationAndLoginApp {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame( "Menu" );
+        frame.setIconImage(Toolkit.getDefaultToolkit().getImage(RegistrationAndLoginApp.class.getResource("/images/Saint_Louis_University_(Baguio)_logo.jpg")));
+        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+
+        frame.getContentPane().add( new RegistrationScreen() );
+
+        frame.pack();
+        frame.setVisible( true );
+    }
 }
